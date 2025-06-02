7ba70a61bcbd848826b72b2aa58159069239d6fb
Aayush Karki <iamaayus001@gmail.com>
2025-06-02 17:58:22 +0545

    Initial Commit - Set up Express & Database Connection

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE_URL.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DB connected succesfully....");
+    app.listen(PORT, () => {
+      console.log("Server connected successfully....");
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection unsucessful", err.message);
+  });
+
